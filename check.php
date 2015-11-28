d49666759064243eb02255652e96f8a52fb7fb53
Yasin Elisli <e.yasin@code.edu.az>
2015-11-28 03:37:32 -0800

    new ch

--- check.php
+++ check.php
@@ -1,29 +1,39 @@
-﻿<?php
-include ('db.php');
-$loginn = $_POST['username'];
-$parol = $_POST['parol'];
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    <?php
+    include ('db.php');
+    $loginn = $_POST['username'];
+    $parol = $_POST['parol'];
 
-$sql = "SELECT * FROM user WHERE login = \"$loginn\" AND password =\"$parol\"";
-$sorgu = $conn-> query($sql);
+    $sql = "SELECT * FROM user WHERE login = \"$loginn\" AND password =\"$parol\"";
+    $sorgu = $conn-> query($sql);
 
-//login ve parol yoxlanmasi
- if (isset($loginn) && isset($parol)) {
-   if (empty($loginn) || empty($parol)) {
-    echo "<h1>Xanalari bos buraxmayin</h1>";
-  }
-  elseif(!empty($loginn) && !empty($parol)) {
-    $user = $sorgu->fetch_assoc();
-     if($user != null){
-        extract($user);
-        // echo $login."<br>".$password."<br>";
-       if ($loginn == $login && $parol == $password) {
-           //$login ve $password cedveldeki sutunun adidir
-             echo "<h1>Xos gelmisiniz</h1>";
+    //login ve parol yoxlanmasi
+     if (isset($loginn) && isset($parol)) {
+       if (empty($loginn) || empty($parol)) {
+        echo "<h1>Xanalari bos buraxmayin</h1>";
+      }
+      elseif(!empty($loginn) && !empty($parol)) {
+        $user = $sorgu->fetch_assoc();
+         if($user != null){
+            extract($user);
+            // echo $login."<br>".$password."<br>";
+           if ($loginn == $login && $parol == $password) {
+               //$login ve $password cedveldeki sutunun adidir
+                 echo "<h1>Xos gelmisiniz</h1>";
+                }
+            }  else {
+              echo "<h1>Login ve ya parol yanlisdir</h1>";
             }
-        }  else {
-          echo "<h1>Login ve ya parol yanlisdir</h1>";
-        }
-   }
- }
-$conn->close();
- ?>
+       }
+     }
+    $conn->close();
+     ?>
+  </body>
+</html>
